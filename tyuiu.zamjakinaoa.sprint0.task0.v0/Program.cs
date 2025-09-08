562e576815fcc293fb8d31d9226c49bf6fac0e13
Shakal <Shakal@DESKTOP-QAV7C1H>
2025-09-08 22:24:10 +0500

    Add project files.

---
+++ tyuiu.zamjakinaoa.sprint0.task0.v0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.zamjakinaoa.sprint0.task0.v0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
