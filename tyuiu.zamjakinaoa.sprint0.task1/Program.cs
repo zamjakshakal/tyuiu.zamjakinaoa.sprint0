4658ff35610a9d459955806253a31605e295d43f
Олеся Замякина <tyuiu.zamjakinaoa@outlook.com>
2025-09-14 12:10:39 +0500

    Task1

---
+++ tyuiu.zamjakinaoa.sprint0.task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.zamjakinaoa.sprint0.task1.v0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Олеся";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
