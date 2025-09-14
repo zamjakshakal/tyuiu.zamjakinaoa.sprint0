e7fcabed21f8354413f21557b4825adc292733fe
Олеся Замякина <tyuiu.zamjakinaoa@outlook.com>
2025-09-15 01:07:54 +0500

    task5

---
+++ tyuiu.zamjakinaoa.sprint0.task5.v0.lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.zamjakinaoa.sprint0.task5.v0.lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Переменная b = {0} на ноль делить нельзя", b);
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+    }
+}
---
+++ tyuiu.zamjakinaoa.sprint0.task5.v0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.zamjakinaoa.sprint0.task5.v0.lib;
+
+namespace tyuiu.zamjakinaoa.sprint0.task5.v0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Substraction(15, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("A / B = " + DataService.Division(9, 3));
+
+            Console.ReadKey();
+        }
+    }
+}
